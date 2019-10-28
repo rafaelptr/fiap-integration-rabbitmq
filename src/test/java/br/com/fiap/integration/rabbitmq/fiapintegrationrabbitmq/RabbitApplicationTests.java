b21a4156f172a8af14720d8a45e8e6c5fc100e87
Rafael Petronilio <rafaelpetronilio@gmail.com>
2019-10-28 20:38:37 -0300

    initial application

---
+++ src/test/java/br/com/fiap/integration/rabbitmq/fiapintegrationrabbitmq/RabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.integration.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
